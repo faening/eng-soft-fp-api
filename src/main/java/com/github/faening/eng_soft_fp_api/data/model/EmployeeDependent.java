7b2914a98ce3ff918d6f23eb497193e961577eec
faening <cmorais58@gmail.com>
2024-03-24 18:29:50 -0300

    PRL-002!feature: add employee dependent entity and migrations

---
+++ src/main/java/com/github/faening/eng_soft_fp_api/data/model/EmployeeDependent.java
@@ -0,0 +1,27 @@
+package com.github.faening.eng_soft_fp_api.data.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "employee_dependent")
+public record EmployeeDependent(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_employee_dependent")
+    Integer id,
+
+    @Embedded
+    Person person,
+
+    @Column(name = "special_needs", nullable = false)
+    Boolean specialNeeds,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "employee_id", referencedColumnName = "id_employee", nullable = false)
+    Employee employee,
+
+    @Embedded
+    EntityMetadata entityMetadata
+) implements Serializable { }
