6b0cce52fba038df2f290c310a64da64c3bc11aa
faening <cmorais58@gmail.com>
2024-03-24 11:39:24 -0300

    first commit

---
+++ src/main/java/com/github/faening/eng_soft_fp_api/Application.java
@@ -0,0 +1,12 @@
+package com.github.faening.eng_soft_fp_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
---
+++ src/test/java/com/github/faening/eng_soft_fp_api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.faening.eng_soft_fp_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
