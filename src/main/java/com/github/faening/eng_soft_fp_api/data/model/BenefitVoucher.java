6229f8aa33f9ec31d74dd820b417408c77e86d90
faening <cmorais58@gmail.com>
2024-03-25 14:48:28 -0300

    PRL-002!feature: add benefit voucher entity and migration

---
+++ src/main/java/com/github/faening/eng_soft_fp_api/data/model/BenefitVoucher.java
@@ -0,0 +1,45 @@
+package com.github.faening.eng_soft_fp_api.data.model;
+
+import com.github.faening.eng_soft_fp_api.domain.enumeration.ApprovalStatus;
+import com.github.faening.eng_soft_fp_api.domain.enumeration.BenefitVoucherType;
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "benefit_voucher")
+public record BenefitVoucher(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_benefit_voucher")
+    Integer id,
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id", referencedColumnName = "id_employee", nullable = false)
+    Employee employee,
+
+    @Column(name = "paid_value", nullable = false)
+    Double paidValue,
+
+    @Column(name = "release_date", nullable = false)
+    @Temporal(TemporalType.DATE)
+    Date releaseDate,
+
+    @Column(name = "description", length = 120, nullable = false)
+    String description,
+
+    @Column(name = "benefit_type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    BenefitVoucherType benefitType,
+
+    @Column(name = "status", nullable = false, columnDefinition = "DEFAULT 'PENDING'")
+    @Enumerated(EnumType.STRING)
+    ApprovalStatus status,
+
+    @Column(name = "payroll_deductible", nullable = false, columnDefinition = "DEFAULT FALSE")
+    Boolean payrollDeductible,
+
+    @Embedded
+    EntityMetadata entityMetadata
+) implements Serializable { }
---
+++ src/main/resources/db/migration/V13__Create_Table_BenefitVoucher.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS benefit_voucher (
+    id_benefit_voucher INT NOT NULL AUTO_INCREMENT,
+    employee_id INT NOT NULL,
+    paid_value DOUBLE NOT NULL,
+    release_date DATE NOT NULL,
+    description VARCHAR(120) NOT NULL,
+    benefit_type ENUM('FOOD', 'TRANSPORT') NOT NULL,
+    status ENUM('PENDING', 'APPROVED', 'DENIED') NOT NULL DEFAULT 'PENDING',
+    payroll_deductible BOOLEAN NOT NULL DEFAULT FALSE,
+    PRIMARY KEY (id_benefit_voucher),
+    FOREIGN KEY (employee_id) REFERENCES employee(id_employee)
+);
