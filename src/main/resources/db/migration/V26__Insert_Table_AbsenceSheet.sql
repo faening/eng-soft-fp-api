94958927f56379c5aebc5dafcae572275b63007d
faening <cmorais58@gmail.com>
2024-03-26 19:04:56 -0300

    PRL-002!refactor: update absence sheet

--- src/main/resources/db/migration/V26__Insert_Table_AbsenceSheet.sql
+++ src/main/resources/db/migration/V26__Insert_Table_AbsenceSheet.sql
@@ -1,8 +1,12 @@
 INSERT INTO absence_sheet
     (employee_id, type, start_date, end_date, observation, status)
 VALUES
-    -- Licença Maternidade
+    -- Dia de Folga
     (37, 'MATERNITY_LEAVE', '2023-10-25', '2024-04-25', 'Z34', 'APPROVED'),
+
+    -- Licença Maternidade
+    (46, 'DAY_OFF', '2023-03-22', '2024-03-22', 'DAY OFF', 'APPROVED'),
+    (20, 'DAY_OFF', '2023-03-11', '2024-03-11', 'DAY OFF', 'APPROVED'),
 
     -- Falta Não Justificada
     (24, 'ABSENCE_WITHOUT_JUSTIFICATION', '2024-03-05', '2024-03-05', 'NOT JUSTIFIED', 'APPROVED'),
