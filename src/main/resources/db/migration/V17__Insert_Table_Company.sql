286ea13e19953fa2efedee3a1927e0ca5c8fcc08
faening <cmorais58@gmail.com>
2024-03-25 23:49:07 -0300

    PRL-002!feature: populate company onto database

---
+++ src/main/resources/db/migration/V17__Insert_Table_Company.sql
@@ -0,0 +1,4 @@
+INSERT INTO company
+    (corporate_name, trade_name, cnpj, ie, opening_date, phone, email, address_street, address_number, address_complement, address_city, address_uf, address_zip_code)
+VALUES
+   ('WRA SOLUÇÕES EM TECNOLOGIA LTDA', 'WRA TECH', "45669110000189", "2785322080", '2019-09-03', "4429690456", 'WRATECH@SAC.COM.BR', 'RUA JOÃO RAMALHO FILHO', '196', 'SALA 2', 'CASCAVEL', 'PR', '85805110');
