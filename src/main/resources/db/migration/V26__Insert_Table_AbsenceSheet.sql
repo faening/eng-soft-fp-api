720f740edc852fedd03345f3fc826ff89d009df0
faening <cmorais58@gmail.com>
2024-03-26 23:36:45 -0300

    PRL-002!feature: populate hours worked sheet

--- src/main/resources/db/migration/V26__Insert_Table_AbsenceSheet.sql
+++ src/main/resources/db/migration/V26__Insert_Table_AbsenceSheet.sql
@@ -1,15 +1,15 @@
 INSERT INTO absence_sheet
     (employee_id, type, start_date, end_date, observation, status)
 VALUES
-    -- Dia de Folga
+    -- Licença Maternidade
     (37, 'MATERNITY_LEAVE', '2023-10-25', '2024-04-25', 'Z34', 'APPROVED'),
 
-    -- Licença Maternidade
+    -- Dia de Folga
     (46, 'DAY_OFF', '2023-03-22', '2024-03-22', 'DAY OFF', 'APPROVED'),
     (20, 'DAY_OFF', '2023-03-11', '2024-03-11', 'DAY OFF', 'APPROVED'),
 
     -- Falta Não Justificada
-    (24, 'ABSENCE_WITHOUT_JUSTIFICATION', '2024-03-05', '2024-03-05', 'NOT JUSTIFIED', 'APPROVED'),
+    (23, 'ABSENCE_WITHOUT_JUSTIFICATION', '2024-03-05', '2024-03-05', 'NOT JUSTIFIED', 'APPROVED'),
     (48, 'ABSENCE_WITHOUT_JUSTIFICATION', '2024-03-15', '2024-03-15', 'NOT JUSTIFIED', 'APPROVED');
 
 
