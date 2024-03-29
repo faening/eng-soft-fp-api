8f9051cbd12980d032b8930e936133c4414e826c
faening <cmorais58@gmail.com>
2024-03-29 18:57:29 -0300

    PRL-002!refactor: update greeting controller

--- src/main/java/com/github/faening/eng_soft_fp_api/controller/GreetingController.java
+++ src/main/java/com/github/faening/eng_soft_fp_api/controller/GreetingController.java
@@ -1,5 +1,6 @@
 package com.github.faening.eng_soft_fp_api.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,14 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/v1/greeting")
 public class GreetingController {
     private record Greeting(long id, String content) { }
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @GetMapping("greeting")
-    private Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    @GetMapping(
+        produces = MediaType.APPLICATION_JSON_VALUE
+    ) private Greeting greeting(
+        @RequestParam(value = "name", defaultValue = "World") String name
+    ) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
