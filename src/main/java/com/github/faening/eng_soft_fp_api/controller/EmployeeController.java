98d7247ad981d62e94970c8c5aa185a3bd29181e
faening <cmorais58@gmail.com>
2024-04-14 23:14:15 -0300

    PRL-003!feature: add employee layer structure

--- src/main/java/com/github/faening/eng_soft_fp_api/controller/EmployeeController.java
+++ src/main/java/com/github/faening/eng_soft_fp_api/controller/EmployeeController.java
@@ -1,5 +1,7 @@
 package com.github.faening.eng_soft_fp_api.controller;
 
+import com.github.faening.eng_soft_fp_api.domain.model.employee.EmployeeRequestDTO;
+import com.github.faening.eng_soft_fp_api.domain.model.employee.EmployeeResponseDTO;
 import com.github.faening.eng_soft_fp_api.domain.model.employee.EmployeeSummaryDTO;
 import com.github.faening.eng_soft_fp_api.domain.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,26 +12,31 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SuppressWarnings("unused")
 @RestController
 @RequestMapping("/v1/employees")
-public class EmployeeController {
-    private final EmployeeService employeeService;
+public class EmployeeController extends AbstractController<EmployeeRequestDTO, EmployeeResponseDTO> {
+    private final EmployeeService service;
 
     @Autowired
-    public EmployeeController(EmployeeService employeeService) {
-        this.employeeService = employeeService;
+    public EmployeeController(EmployeeService service) {
+        super(service);
+        this.service = service;
     }
 
-    @GetMapping(
-        value = { "summary/{id}" },
-        produces = MediaType.APPLICATION_JSON_VALUE
-    )
+    @GetMapping(value = { "/summary" }, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<EmployeeSummaryDTO>> getAllEmployeeSummaries() {
+        List<EmployeeSummaryDTO> departmentResponseDTOs = service.getAllSummaries();
+        return ResponseEntity.ok(departmentResponseDTOs);
+    }
+
+    @GetMapping(value = { "/summary/{id}" }, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<EmployeeSummaryDTO> getEmployeeSummaryById(
         @PathVariable(value = "id") Integer id
     ) {
-        EmployeeSummaryDTO departmentResponseDTO = employeeService.getSummaryById(id);
+        EmployeeSummaryDTO departmentResponseDTO = service.getSummaryById(id);
         return ResponseEntity.ok(departmentResponseDTO);
     }
-
 }
