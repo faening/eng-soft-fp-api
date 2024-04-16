102d878a685f6af35304c89d98da33281a6789da
faening <cmorais58@gmail.com>
2024-04-16 17:18:50 -0300

    PRL-008!docs: add generic worked hours calculation

--- src/main/java/com/github/faening/eng_soft_fp_api/domain/calculation/income/CalculateRegularHours.java
+++ src/main/java/com/github/faening/eng_soft_fp_api/domain/calculation/income/CalculateRegularHours.java
@@ -1,11 +1,12 @@
 package com.github.faening.eng_soft_fp_api.domain.calculation.income;
 
+import com.github.faening.eng_soft_fp_api.config.WorkingHoursConfig;
 import com.github.faening.eng_soft_fp_api.domain.calculation.CalculationParameters;
 import com.github.faening.eng_soft_fp_api.domain.calculation.PayrollCalculation;
+import com.github.faening.eng_soft_fp_api.domain.calculation.generics.WorkedHoursCalculation;
 import com.github.faening.eng_soft_fp_api.domain.model.hours_worked_sheet.HoursWorkedSheetResponseDTO;
 import com.github.faening.eng_soft_fp_api.domain.model.payroll_item.PayrollItemRequestDTO;
 import com.github.faening.eng_soft_fp_api.domain.model.rubric.RubricResponseDTO;
-import com.github.faening.eng_soft_fp_api.domain.model.work_shift.WorkShiftResponseDTO;
 import com.github.faening.eng_soft_fp_api.domain.service.HoursWorkedSheetService;
 import com.github.faening.eng_soft_fp_api.domain.service.RubricService;
 import com.github.faening.eng_soft_fp_api.domain.service.WorkShiftService;
@@ -15,27 +16,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Objects;
 
 @SuppressWarnings({"unused", "SpellCheckingInspection"})
 @Component
-public class CalculateRegularHours implements PayrollCalculation {
+public class CalculateRegularHours extends WorkedHoursCalculation implements PayrollCalculation {
     private final RubricService rubricService;
-    private final HoursWorkedSheetService hoursWorkedSheetService;
-    private final WorkShiftService workShiftService;
     private final static Integer RUBRIC_CODE = 1000;
 
     @Autowired
     public CalculateRegularHours(
         RubricService rubricService,
         HoursWorkedSheetService hoursWorkedSheetService,
-        WorkShiftService workShiftService
+        WorkShiftService workShiftService,
+        WorkingHoursConfig workingHoursConfig
     ) {
+        super(workShiftService, hoursWorkedSheetService, workingHoursConfig);
         this.rubricService = rubricService;
-        this.hoursWorkedSheetService = hoursWorkedSheetService;
-        this.workShiftService = workShiftService;
     }
 
     @Override
@@ -43,7 +41,7 @@ public class CalculateRegularHours implements PayrollCalculation {
         if (parameters != null) {
             RubricResponseDTO rubric = getRubricByCode();
             Integer workingHoursInMonth = getWorkingHoursInMonth(parameters);
-            List<HoursWorkedSheetResponseDTO> hoursWorkedSheet = getHoursWorkedSheetByEmployeeIdAndDate(parameters);
+            List<HoursWorkedSheetResponseDTO> hoursWorkedSheet = getHoursWorkedSheet(parameters);
             Integer employeeTotalHours = getEmployeeTotalHours(hoursWorkedSheet);
             BigDecimal employeeCalculatedSalary = calculateEmployeeSalary(parameters.getEmployee().getSalary(), workingHoursInMonth, employeeTotalHours);
 
@@ -58,32 +56,21 @@ public class CalculateRegularHours implements PayrollCalculation {
         return null;
     }
 
-    protected RubricResponseDTO getRubricByCode() {
+    /**
+     * Este método recupera uma rubrica pelo seu código.
+     *
+     * @return Um objeto RubricResponseDTO que representa a rubrica recuperada.
+     */
+    public RubricResponseDTO getRubricByCode() {
         return rubricService.getByCode(RUBRIC_CODE);
     }
 
-    protected Integer getWorkingHoursInMonth(CalculationParameters parameters) {
-        WorkShiftResponseDTO workShift = getWorkShiftByEmployeeId(parameters);
-        // Se a propriedade `reducedShift` for verdadeira, a carga horária é de 6 horas diárias
-        if (workShift != null) {
-            return workShift.getReducedShift()
-                ? 6 * DateUtils.getWorkingDaysInMonth(parameters.getYear(), parameters.getMonth().getValue())
-                : 8 * DateUtils.getWorkingDaysInMonth(parameters.getYear(), parameters.getMonth().getValue());
-        }
-        return null;
-    }
-
-    protected WorkShiftResponseDTO getWorkShiftByEmployeeId(CalculationParameters parameters) {
-        return workShiftService.getById(parameters.getEmployee().getWorkShiftId());
-    }
-
-    protected List<HoursWorkedSheetResponseDTO> getHoursWorkedSheetByEmployeeIdAndDate(CalculationParameters parameters) {
-        LocalDate[] startAndEndDate = DateUtils.getFirstAndLastDayOfMonth(parameters.getYear(), parameters.getMonth().getValue());
-        LocalDate startDate = startAndEndDate[0];
-        LocalDate endDate = startAndEndDate[1];
-        return hoursWorkedSheetService.getWorkedHoursByEmployeeIdAndDateRange(parameters.getEmployee().getId(), startDate, endDate);
-    }
-
+    /**
+     * Este método calcula o total de horas regulares trabalhadas por um funcionário, convertendo o tempo para horas.
+     *
+     * @param hoursWorkedSheet Uma lista de objetos HoursWorkedSheetResponseDTO que representam as horas trabalhadas pelo funcionário.
+     * @return O total de horas regulares trabalhadas pelo funcionário.
+     */
     protected Integer getEmployeeTotalHours(List<HoursWorkedSheetResponseDTO> hoursWorkedSheet) {
         int totalMinutes = hoursWorkedSheet
             .stream()
@@ -92,13 +79,22 @@ public class CalculateRegularHours implements PayrollCalculation {
         return totalMinutes / 60;
     }
 
+    /**
+     * Este método calcula o salário de um funcionário com base nas horas trabalhadas.
+     *
+     * @param salary Salário base do funcionário.
+     * @param workingHoursInMonth Total de horas de trabalho em um mês.
+     * @param employeeTotalHours Total de horas trabalhadas pelo funcionário.
+     * @return O salário calculado do funcionário. Se as horas trabalhadas pelo funcionário forem iguais às horas de trabalho em um mês, retorna o salário base. Caso contrário, calcula o valor da hora e multiplica pelas horas trabalhadas.
+     */
     protected BigDecimal calculateEmployeeSalary(BigDecimal salary, int workingHoursInMonth, int employeeTotalHours) {
-        // Se o total de horas trabalhadas for igual ao total de horas do mês, o salário é retornado
+        // Se as horas trabalhadas pelo funcionário forem iguais às horas de trabalho em um mês, retorna o salário base
         if (Objects.equals(workingHoursInMonth, employeeTotalHours)) {
             return salary;
-        // Caso contrário, o valor da hora do funcionário é multiplicado pela quantidade de horas trabalhadas no mês
         } else {
+            // Calcula o valor da hora com base no salário e nas horas de trabalho em um mês
             BigDecimal hourValue = EmployeeUtils.calculateHourlyRate(salary, workingHoursInMonth);
+            // Multiplica o valor da hora pelas horas trabalhadas pelo funcionário para obter o salário calculado
             return hourValue.multiply(BigDecimal.valueOf(employeeTotalHours));
         }
     }
