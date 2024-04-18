9d6e026c3686711221118c2e873bebbe626f91ab
faening <cmorais58@gmail.com>
2024-04-18 16:50:49 -0300

    PRL-0018!feature: implements the calculation of loan discount

--- src/main/java/com/github/faening/eng_soft_fp_api/domain/calculation/deduction/CalculateLoanDiscount.java
+++ src/main/java/com/github/faening/eng_soft_fp_api/domain/calculation/deduction/CalculateLoanDiscount.java
@@ -2,128 +2,94 @@ package com.github.faening.eng_soft_fp_api.domain.calculation.deduction;
 
 import com.github.faening.eng_soft_fp_api.domain.calculation.CalculationParameters;
 import com.github.faening.eng_soft_fp_api.domain.calculation.PayrollCalculation;
+import com.github.faening.eng_soft_fp_api.domain.enumeration.PaymentStatus;
+import com.github.faening.eng_soft_fp_api.domain.model.loan.LoanResponseDTO;
+import com.github.faening.eng_soft_fp_api.domain.model.loan_installment.LoanInstallmentResponseDTO;
 import com.github.faening.eng_soft_fp_api.domain.model.payroll_item.PayrollItemRequestDTO;
+import com.github.faening.eng_soft_fp_api.domain.model.rubric.RubricResponseDTO;
+import com.github.faening.eng_soft_fp_api.domain.service.LoanService;
+import com.github.faening.eng_soft_fp_api.domain.service.RubricService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-/*
- * Requisito: [RD013] Calcular Desconto Empréstimo (Adiantamento Salário)
- *
- * Descrição:
- * Esta classe é responsável por calcular o desconto por empréstimos ou adiantamentos de um funcionário em um determinado mês.
- *
- *
- * Funcionamento:
- * Para realizar os cálculos, esta classe observa as tabelas `loan` e `installment`.
- * O desconto por empréstimos é calculado com base no valor da parcela do mês atual.
- *
- * Observe que um funcionário pode ter mais de um empréstimo, e o desconto por empréstimos deve o total dos valores das parcelas de todos os
- * empréstimos no mês.
- * Observe que o desconto só pode ocorrer se a propriedade `loan.company_payment_status` for `PAID` (pago).
- *
- * Exemplos:
- * ...
- */
+import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
-@SuppressWarnings("unused")
+@SuppressWarnings({"unused", "SpellCheckingInspection"})
+@Component
 public class CalculateLoanDiscount implements PayrollCalculation {
-    /*
-     * Dicas de codificação:
-     *
-     * Dica 1:
-     * Sempre crie uma branch separada para desenvolver uma funcionalidade. Isso permite que você trabalhe em um ambiente isolado e evita
-     * conflitos com o código de seus colegas.
-     *
-     * Outro ponto importante é que, a branch de origem sempre deve ser a `develop`.
-     *
-     * Lembre-se de seguir o padrão de nomenclatura de branches. Por exemplo: PRL-001, PRL-002, PRL-003, etc. Observe isso na respectiva
-     * tarefa do Trello.
-     *
-     *
-     *
-     * Dica 2:
-     * Sempre busque a separação de responsabilidades. Se você perceber que o método está fazendo mais de uma coisa, considere dividí-lo em
-     * métodos menores. Isso facilita a leitura e a manutenção do código. Por exemplo:
-     *
-     * public void calculate(CalculationParameters parameters) {
-     *   metodo1();
-     *   metodo2();
-     *   return ...
-     * }
-     *
-     *
-     *
-     * Dica 3:
-     * Evite aninhar if's. Se você perceber que isso está acontecendo, considere decompor o código ou criar métodos menores. Por exemplo:
-     *
-     * if (condicao1) {
-     *    if (condicao2) {
-     *      ...
-     *   }
-     * }
-     *
-     * Pode ser decomposto em:
-     *
-     * if (condicao1 && condicao2) {
-     *   ...
-     * }
-     *
-     * ou
-     *
-     * if (condicao1) {
-     *   metodo1();
-     * }
-     *
-     * if (condicao2) {
-     *   metodo2();
-     * }
-     *
-     *
-     *
-     * Dica 4:
-     * Evite duplicação de código. Se você perceber que um trecho de código está sendo repetido, considere reduzi-lo para evitar a duplicação.
-     * Por exemplo:
-     *
-     * if (condicao1) {
-     *   metodo1();
-     * }
-     *
-     * if (condicao2) {
-     *   metodo1();
-     * }
-     *
-     * Pode ser decomposto em:
-     *
-     * if (condicao1 || condicao2) {
-     *   metodo1();
-     * }
-     *
-     *
-     *
-     * Dica 5:
-     * Não se esqueça de tratar os casos de erro. Se algo der errado, retorne uma exceção. Sempre que possível, faça a verificação das
-     * condições de erro no início do método. Por exemplo:
-     *
-     * if (condicao1) {
-     *   if (condicao1 == null) throw new RuntimeException("Mensagem de erro");
-     *   ...
-     * }
-     *
-     *
-     * Dica 6:
-     * Não se esqueça de testar o seu código. Testes de unidade são uma ótima forma de garantir que o seu código está funcionando. Se você
-     * não sabe como fazer testes de unidade, procure aprender ou use o ChatGPT. Eles são muito importantes para garantir a qualidade do seu
-     * código.
-     *
-     *
-     * Dica 7:
-     * Atenção ao retorno do método calculate. Ele deve retornar um objeto do tipo PayrollItemRequestDTO.
-     *
-     *
-     *
-     * Dica 8:
-     * Por fim e não menos importante, ao terminar sua implementação, remova esses comentários e faça o commit e push do código.
-     * */
+    private final RubricService rubricService;
+    private final LoanService loanService;
+    private final static Integer RUBRIC_CODE = 9200;
+
+    @Autowired
+    public CalculateLoanDiscount(
+        RubricService rubricService,
+        LoanService loanService
+    ) {
+        this.rubricService = rubricService;
+        this.loanService = loanService;
+    }
+
     @Override
     public PayrollItemRequestDTO calculate(CalculationParameters parameters) {
-        return null;
+        return Optional.ofNullable(parameters)
+            .map(params -> {
+                BigDecimal loanInstallments = getLoanInstallments(params);
+                return loanInstallments.compareTo(BigDecimal.ZERO) > 0 ?
+                    new PayrollItemRequestDTO(
+                        getRubricByCode(),
+                        null,
+                        loanInstallments,
+                        loanInstallments,
+                        BigDecimal.ZERO
+                    ) : null;
+            })
+            .orElse(null);
+    }
+
+    /**
+     * Este método recupera uma rubrica pelo seu código.
+     *
+     * @return Um objeto RubricResponseDTO que representa a rubrica recuperada.
+     */
+    public RubricResponseDTO getRubricByCode() {
+        return rubricService.getByCode(RUBRIC_CODE);
+    }
+
+    /**
+     * Este método calcula o valor total das parcelas de empréstimo pendentes de um funcionário em um determinado mês.
+     *
+     * @param parameters Um objeto CalculationParameters que contém os parâmetros necessários para o cálculo.
+     * @return Um BigDecimal que representa o valor total das parcelas de empréstimo pendentes.
+     */
+    protected BigDecimal getLoanInstallments(CalculationParameters parameters) {
+        if (parameters == null) {
+            return null;
+        }
+
+        // Recupera os empréstimos do funcionário com parcelas pendentes no mês especificado
+        List<LoanResponseDTO> loans = loanService.getLoanByEmployeeIdAndSpecs(
+            parameters.getEmployee().getId(),
+            null,
+            null,
+            null,
+            PaymentStatus.PAID,
+            PaymentStatus.RELEASED
+        );
+
+        // Calcula o valor total das parcelas de empréstimo pendentes no mês especificado.
+        // Se houver parcelas pendentes, retorna a soma dos valores das parcelas pendentes. Caso contrário, retorna zero.
+        return Optional.ofNullable(loans)
+            .orElse(Collections.emptyList())
+            .stream()
+            .flatMap(loan -> Optional.ofNullable(loan.getInstallments()).orElse(Collections.emptyList()).stream())
+            .filter(installment -> installment.getDiscountMonth().equals(parameters.getMonth())
+                 && installment.getPaymentStatus().equals(PaymentStatus.PENDING))
+            .map(LoanInstallmentResponseDTO::getInstallmentValue)
+            .reduce(BigDecimal::add)
+            .orElse(BigDecimal.ZERO);
     }
 }
