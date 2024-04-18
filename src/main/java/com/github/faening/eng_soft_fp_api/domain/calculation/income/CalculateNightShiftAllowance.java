5644a471f35b761cfdbf74d44c3a3f41c1d05db1
faening <cmorais58@gmail.com>
2024-04-18 08:32:39 -0300

    refactor: centralizes logic to calculate night hours

--- src/main/java/com/github/faening/eng_soft_fp_api/domain/calculation/income/CalculateNightShiftAllowance.java
+++ src/main/java/com/github/faening/eng_soft_fp_api/domain/calculation/income/CalculateNightShiftAllowance.java
@@ -3,7 +3,6 @@ package com.github.faening.eng_soft_fp_api.domain.calculation.income;
 import com.github.faening.eng_soft_fp_api.config.WorkingHoursConfig;
 import com.github.faening.eng_soft_fp_api.domain.calculation.CalculationParameters;
 import com.github.faening.eng_soft_fp_api.domain.calculation.PayrollCalculation;
-import com.github.faening.eng_soft_fp_api.domain.calculation.generics.NightShiftCalculation;
 import com.github.faening.eng_soft_fp_api.domain.calculation.generics.WorkedHoursCalculation;
 import com.github.faening.eng_soft_fp_api.domain.model.employee.EmployeeSummaryDTO;
 import com.github.faening.eng_soft_fp_api.domain.model.payroll_item.PayrollItemRequestDTO;
@@ -19,6 +18,8 @@ import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Optional;
 
 @SuppressWarnings({"unused", "SpellCheckingInspection"})
@@ -27,7 +28,10 @@ public class CalculateNightShiftAllowance extends WorkedHoursCalculation impleme
     private final RubricService rubricService;
     private final TaxOrValueService taxOrValueService;
     private final WorkShiftService workShiftService;
+
     private final static Integer RUBRIC_CODE = 1205;
+    private static final LocalTime NIGHT_START = LocalTime.of(22, 0);
+    private static final LocalTime NIGHT_END = LocalTime.of(5, 0);
 
     @Autowired
     public CalculateNightShiftAllowance(
@@ -113,16 +117,87 @@ public class CalculateNightShiftAllowance extends WorkedHoursCalculation impleme
         return Optional.ofNullable(workShift)
             .map(shift -> {
                 // Calcula as horas noturnas para o primeiro período do turno
-                int nightHoursFirstPeriod = NightShiftCalculation.calculateNightHours(
+                int nightHoursFirstPeriod = calculateNightHours(
                     shift.getStartOfWorkday(), shift.getStartOfBreak()
                 );
                 // Calcula as horas noturnas para o segundo período do turno
-                int nightHoursSecondPeriod = NightShiftCalculation.calculateNightHours(
+                int nightHoursSecondPeriod = calculateNightHours(
                     shift.getEndOfBreak(), shift.getEndOfWorkday()
                 );
                 return nightHoursFirstPeriod + nightHoursSecondPeriod;
             })
             .orElse(0);
+    }
+
+    /**
+     * Este método calcula o número de horas noturnas trabalhadas em um turno.
+     * As horas noturnas são definidas como o período entre 22:00 e 05:00.
+     *
+     * @param shiftStart A hora de início do turno.
+     * @param shiftEnd A hora de término do turno.
+     * @return O número de horas noturnas trabalhadas.
+     */
+    public static int calculateNightHours(LocalTime shiftStart, LocalTime shiftEnd) {
+        // Se o turno começa e termina fora do período noturno, retorna 0
+        if (shiftStart.isBefore(NIGHT_START) && shiftStart.isAfter(NIGHT_END) ||
+            shiftEnd.isBefore(NIGHT_START) && shiftEnd.isAfter(NIGHT_END) && shiftStart.isAfter(NIGHT_START)) {
+            return 0;
+        }
+        // Se o turno começa e termina no mesmo dia, calcula as horas noturnas para o mesmo dia
+        if (shiftStart.isBefore(shiftEnd)) {
+            return calculateNightHoursSameDay(shiftStart, shiftEnd);
+            // Se o turno começa em um dia e termina no próximo, calcula as horas noturnas para o turno que se estende pela meia-noite
+        } else {
+            return calculateNightHoursOvernight(shiftStart, shiftEnd);
+        }
+    }
+
+    /**
+     * Este método calcula o número de horas noturnas trabalhadas em um turno que começa e termina no mesmo dia.
+     * As horas noturnas são definidas como o período entre 22:00 e 05:00.
+     *
+     * @param shiftStart A hora de início do turno.
+     * @param shiftEnd A hora de término do turno.
+     * @return O número de horas noturnas trabalhadas.
+     */
+    private static int calculateNightHoursSameDay(LocalTime shiftStart, LocalTime shiftEnd) {
+        // Define o início do período noturno. Se o turno começa antes do início do período noturno, o início do período noturno é considerado.
+        // Se o turno começa depois do fim do período noturno, o início do turno é considerado.
+        LocalTime nightStart = (shiftStart.isBefore(NIGHT_START) && shiftStart.isAfter(NIGHT_END)) ? NIGHT_START : shiftStart;
+        // Define o fim do período noturno. Se o turno termina depois do fim do período noturno, o fim do período noturno é considerado.
+        // Se o turno termina antes do início do período noturno, o fim do turno é considerado.
+        LocalTime nightEnd = (shiftEnd.isAfter(NIGHT_END) && shiftEnd.isBefore(NIGHT_START)) ? NIGHT_END : shiftEnd;
+
+        // Se o turno começa depois do fim do período noturno e termina antes do início do período noturno, o início e o fim do turno são considerados.
+        if (shiftStart.isAfter(NIGHT_END) && shiftEnd.isBefore(NIGHT_START)) {
+            nightStart = shiftStart;
+            nightEnd = shiftEnd;
+        }
+
+        long minutes = ChronoUnit.MINUTES.between(nightStart, nightEnd);
+        return (int) Math.ceil(minutes / 60.0);
+    }
+
+    /**
+     * Este método calcula o número de horas noturnas trabalhadas em um turno que começa em um dia e termina no próximo.
+     * As horas noturnas são definidas como o período entre 22:00 e 05:00.
+     *
+     * @param shiftStart A hora de início do turno.
+     * @param shiftEnd A hora de término do turno.
+     * @return O número de horas noturnas trabalhadas.
+     */
+    private static int calculateNightHoursOvernight(LocalTime shiftStart, LocalTime shiftEnd) {
+        // Define o início do período noturno antes da meia-noite. Se o turno começa antes do início do período noturno, o início do período noturno é considerado.
+        LocalTime nightStartBeforeMidnight = shiftStart.isBefore(NIGHT_START) ? NIGHT_START : shiftStart;
+        long minutesBeforeMidnight = ChronoUnit.MINUTES.between(nightStartBeforeMidnight, LocalTime.MAX);
+        int nightHoursBeforeMidnight = (int) Math.ceil(minutesBeforeMidnight / 60.0);
+
+        // Define o fim do período noturno depois da meia-noite. Se o turno termina depois do fim do período noturno, o fim do período noturno é considerado.
+        LocalTime nightEndAfterMidnight = shiftEnd.isAfter(NIGHT_END) ? NIGHT_END : shiftEnd;
+        long minutesAfterMidnight = ChronoUnit.MINUTES.between(LocalTime.MIN, nightEndAfterMidnight);
+        int nightHoursAfterMidnight = (int) Math.ceil(minutesAfterMidnight / 60.0);
+
+        return nightHoursBeforeMidnight + nightHoursAfterMidnight;
     }
 
     /**
