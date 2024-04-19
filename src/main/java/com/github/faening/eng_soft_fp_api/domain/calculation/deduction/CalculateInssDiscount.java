8b1b09af134d0e4e8657bcfb2cac92d8c16e4b7e
faening <cmorais58@gmail.com>
2024-04-19 15:07:15 -0300

    PRL-0019!feature: implements the calculation of INSS

--- src/main/java/com/github/faening/eng_soft_fp_api/domain/calculation/deduction/CalculateInssDiscount.java
+++ src/main/java/com/github/faening/eng_soft_fp_api/domain/calculation/deduction/CalculateInssDiscount.java
@@ -2,125 +2,68 @@ package com.github.faening.eng_soft_fp_api.domain.calculation.deduction;
 
 import com.github.faening.eng_soft_fp_api.domain.calculation.CalculationParameters;
 import com.github.faening.eng_soft_fp_api.domain.calculation.PayrollCalculation;
+import com.github.faening.eng_soft_fp_api.domain.model.employee.EmployeeSummaryDTO;
 import com.github.faening.eng_soft_fp_api.domain.model.payroll_item.PayrollItemRequestDTO;
+import com.github.faening.eng_soft_fp_api.domain.model.rubric.RubricResponseDTO;
+import com.github.faening.eng_soft_fp_api.domain.model.tax_or_value.TaxOrValueResponseDTO;
+import com.github.faening.eng_soft_fp_api.domain.service.RubricService;
+import com.github.faening.eng_soft_fp_api.domain.service.TaxOrValueService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-/*
- * Requisito: [RD014] Calcular Desconto INSS
- *
- * Descrição:
- * Esta classe é responsável por calcular o desconto de INSS de um funcionário em um determinado mês.
- *
- *
- * Funcionamento:
- * Para realizar os cálculos, esta classe observa as propriedades `employee.salary` e `tax_or_value` com o `type`: `INSS`.
- * O desconto de INSS é calculado sobre o salário base do funcionário no mês. O valor é armazenado na tabela `tax_or_value` com o `type`:
- * `INSS`.
- *
- * Exemplos:
- * ...
- */
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Optional;
 
-@SuppressWarnings("unused")
+@SuppressWarnings({"unused", "SpellCheckingInspection"})
+@Component
 public class CalculateInssDiscount implements PayrollCalculation {
-    /*
-     * Dicas de codificação:
-     *
-     * Dica 1:
-     * Sempre crie uma branch separada para desenvolver uma funcionalidade. Isso permite que você trabalhe em um ambiente isolado e evita
-     * conflitos com o código de seus colegas.
-     *
-     * Outro ponto importante é que, a branch de origem sempre deve ser a `develop`.
-     *
-     * Lembre-se de seguir o padrão de nomenclatura de branches. Por exemplo: PRL-001, PRL-002, PRL-003, etc. Observe isso na respectiva
-     * tarefa do Trello.
-     *
-     *
-     *
-     * Dica 2:
-     * Sempre busque a separação de responsabilidades. Se você perceber que o método está fazendo mais de uma coisa, considere dividí-lo em
-     * métodos menores. Isso facilita a leitura e a manutenção do código. Por exemplo:
-     *
-     * public void calculate(CalculationParameters parameters) {
-     *   metodo1();
-     *   metodo2();
-     *   return ...
-     * }
-     *
-     *
-     *
-     * Dica 3:
-     * Evite aninhar if's. Se você perceber que isso está acontecendo, considere decompor o código ou criar métodos menores. Por exemplo:
-     *
-     * if (condicao1) {
-     *    if (condicao2) {
-     *      ...
-     *   }
-     * }
-     *
-     * Pode ser decomposto em:
-     *
-     * if (condicao1 && condicao2) {
-     *   ...
-     * }
-     *
-     * ou
-     *
-     * if (condicao1) {
-     *   metodo1();
-     * }
-     *
-     * if (condicao2) {
-     *   metodo2();
-     * }
-     *
-     *
-     *
-     * Dica 4:
-     * Evite duplicação de código. Se você perceber que um trecho de código está sendo repetido, considere reduzi-lo para evitar a duplicação.
-     * Por exemplo:
-     *
-     * if (condicao1) {
-     *   metodo1();
-     * }
-     *
-     * if (condicao2) {
-     *   metodo1();
-     * }
-     *
-     * Pode ser decomposto em:
-     *
-     * if (condicao1 || condicao2) {
-     *   metodo1();
-     * }
-     *
-     *
-     *
-     * Dica 5:
-     * Não se esqueça de tratar os casos de erro. Se algo der errado, retorne uma exceção. Sempre que possível, faça a verificação das
-     * condições de erro no início do método. Por exemplo:
-     *
-     * if (condicao1) {
-     *   if (condicao1 == null) throw new RuntimeException("Mensagem de erro");
-     *   ...
-     * }
-     *
-     *
-     * Dica 6:
-     * Não se esqueça de testar o seu código. Testes de unidade são uma ótima forma de garantir que o seu código está funcionando. Se você
-     * não sabe como fazer testes de unidade, procure aprender ou use o ChatGPT. Eles são muito importantes para garantir a qualidade do seu
-     * código.
-     *
-     *
-     * Dica 7:
-     * Atenção ao retorno do método calculate. Ele deve retornar um objeto do tipo PayrollItemRequestDTO.
-     *
-     *
-     *
-     * Dica 8:
-     * Por fim e não menos importante, ao terminar sua implementação, remova esses comentários e faça o commit e push do código.
-     * */
+    private final RubricService rubricService;
+    private final TaxOrValueService taxOrValueService;
+    private final static Integer RUBRIC_CODE = 9201;
+
+    @Autowired
+    public CalculateInssDiscount(RubricService rubricService, TaxOrValueService taxOrValueService) {
+        this.rubricService = rubricService;
+        this.taxOrValueService = taxOrValueService;
+    }
+
     @Override
     public PayrollItemRequestDTO calculate(CalculationParameters parameters) {
-        return null;
+        BigDecimal inssDiscount = calculateInssDiscount(parameters);
+        return inssDiscount.compareTo(BigDecimal.ZERO) > 0
+            ? new PayrollItemRequestDTO(
+            getRubricByCode(),
+            taxOrValueService.getInssPercentageByRange(parameters.getEmployee().getSalary()),
+            parameters.getEmployee().getSalary(),
+            inssDiscount,
+            taxOrValueService.getInssPercentageByRange(parameters.getEmployee().getSalary()).getTaxPercentage()
+        ) : null;
+    }
+
+    /**
+     * Este método recupera uma rubrica pelo seu código.
+     *
+     * @return Um objeto RubricResponseDTO que representa a rubrica recuperada.
+     */
+    public RubricResponseDTO getRubricByCode() {
+        return rubricService.getByCode(RUBRIC_CODE);
+    }
+
+    /**
+     * Este método calcula o desconto de INSS.
+     *
+     * @param parameters Parâmetros de cálculo.
+     * @return O valor do desconto de INSS.
+     */
+    protected BigDecimal calculateInssDiscount(CalculationParameters parameters) {
+        return Optional.ofNullable(parameters)
+            .map(CalculationParameters::getEmployee)
+            .map(EmployeeSummaryDTO::getSalary)
+            .map(salary -> {
+                TaxOrValueResponseDTO inssPercentage = taxOrValueService.getInssPercentageByRange(salary);
+                return salary.multiply(inssPercentage.getTaxPercentage().divide(BigDecimal.valueOf(100), 3, RoundingMode.HALF_UP)).setScale(2, RoundingMode.HALF_UP);
+            })
+            .orElse(BigDecimal.ZERO);
     }
 }
